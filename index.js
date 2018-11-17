5c69c11f0e72ac0fc7fec8212c541546e2eb442e
bethwilson07 <beth.ny.wlsn@gmail.com>
2018-11-17 12:47:57 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "beth"
+const shortGreeting = "Welcome, " + currentUser[0] + "!"
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
+const excitedWelcomeMessage = "Welcome to Flatbook, ".toUpperCase() + currentUser.toUpperCase() + "!"
